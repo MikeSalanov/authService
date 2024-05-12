f66e999d03bedeaf8f4f08fdcf6db64cc0c244cc
Michael <michael@simpleswap.io>
2024-05-12 14:26:26 +0300

    remove confirmPassword validation

--- routers/api.signup.router.js
+++ routers/api.signup.router.js
@@ -2,12 +2,9 @@ const router = require("express").Router();
 const userService = require("../services/userService");
 const { produceMessageOfRequestRegistration } = require('../services/rabbitService');
 
-router.post("/signUp", async (req, res, next) => {
+router.post('/signUp', async (req, res, next) => {
   try {
-    const { email, password, confirmPassword } = req.body;
-    if (password !== confirmPassword) return res.status(400).json({
-      message: 'Password does not match to confirmPassword'
-    });
+    const { email, password } = req.body;
     const { confirmationCode } = await userService.register(email, password);
     await produceMessageOfRequestRegistration(email, confirmationCode);
     return res.status(201).json({ message: "User has been successfully created" });
