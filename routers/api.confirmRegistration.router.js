fc5cbaefa654a19a5b25b78c0490c4e23deea899
Michael <michael@simpleswap.io>
2024-05-14 18:11:55 +0300

    add produce message to USER_REGISTERED queue

--- routers/api.confirmRegistration.router.js
+++ routers/api.confirmRegistration.router.js
@@ -1,12 +1,12 @@
 const userService = require("../services/userService");
-
+const rabbitService = require("../services/rabbitService");
 const router = require("express").Router();
 
 router.post('/confirm-registration', async (req, res, next) => {
     try {
         const { email, password, confirmationCode } = req.body
         const userData = await userService.confirmRegistration(email, password, confirmationCode);
-
+        await rabbitService.produceMessageOfWalletCreation(userData.user.user_id);
         res.cookie("refreshToken", userData.refreshToken, {
           maxAge: 15 * 60 * 1000,
           httpOnly: true,
--- services/rabbitService.js
+++ services/rabbitService.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 async function produceMessageOfRequestRegistration(email, confirmationCode) {
   const queueName = process.env.REQUEST_REGISTRATION_QUEUE_NAME || 'REGISTRATION_REQUEST';
   try {
-    const connectToQueue = await amqp.connect(process.env.RABBITMQ_ADDRESS);
+    const connectToQueue = await amqp.connect(process.env.RABBIT_URL);
     const channel = await connectToQueue.createChannel();
     await channel.assertQueue(queueName, { durable: true });
     channel.sendToQueue(queueName, Buffer.from(JSON.stringify({
@@ -18,6 +18,23 @@ async function produceMessageOfRequestRegistration(email, confirmationCode) {
   }
 }
 
+async function produceMessageOfWalletCreation(user_id) {
+  const queueName = process.env.USER_REGISTERED_QUEUE_NAME || 'USER_REGISTERED';
+  try {
+    const connectToQueue = await amqp.connect(process.env.RABBIT_URL);
+    const channel = await connectToQueue.createChannel();
+    await channel.assertQueue(queueName, { durable: true });
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify({
+      message: 'New user has been registered',
+      user_id
+    })));
+  } catch (err) {
+    console.log('Error in produce message of USER_REGISTERED to queue', err);
+    throw new Error(err);
+  }
+}
+
 module.exports = {
-  produceMessageOfRequestRegistration
+  produceMessageOfRequestRegistration,
+  produceMessageOfWalletCreation
 };
