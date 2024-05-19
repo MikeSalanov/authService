3cfadeea6a5233c4e21c485c740a3bb4e86fb275
Michael <michael@simpleswap.io>
2024-05-20 01:17:07 +0300

    add .env.example

--- services/tokenService.js
+++ services/tokenService.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const { Token } = require('../db/models');
 
 const generateTokens = (payload) => {
+  console.log(process.env.JWT_ACCESS_SECRET, process.env.JWT_REFRESH_SECRET)
   const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
     expiresIn: "5m",
   });
